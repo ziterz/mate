86ac3917602e5892be2c74f235301eee10d897cb
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2020-03-11 17:22:25 +0700

    edit about page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/mate-logo.png" width="200px">
+    <HelloWorld msg="Welcome to Mate App"/>
   </div>
 </template>
 
